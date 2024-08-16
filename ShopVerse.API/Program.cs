8dd556789bb5c68d61ac6a6119d518c8c1a4b0a2
mirmustafaali672 <mirmustafa672@gmail.com>
2024-08-16 16:48:52 +0530

    adding refernece

--- ShopVerse.API/Program.cs
+++ ShopVerse.API/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ShopVerse.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShopVerseDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
